8711887281c76ce0873d499a53346ec214390ea8
Nigth <omnes.theo@gmail.com>
2023-08-06 19:33:28 +0200

    mobile list of chores done

--- src/routes/housework/+page.ts
+++ src/routes/housework/+page.ts
@@ -1,20 +1,10 @@
 import type { PageLoad } from './$types';
-import { api } from './api';
-import type { Guest } from './guest.type';
-
-type GuestResponse = { name: string; when: string };
+import { api } from '../api';
 
 export const load = (async ({ fetch }) => {
 	const chores = await fetch(api.chores);
-	const groceries = await fetch(api.groceries);
-	const guests = await fetch(api.guests).then((res) => res.json());
-	const guestsParsed: Guest[] = guests.map((guest: GuestResponse) => {
-		return { name: guest.name, when: new Date(guest.when) };
-	});
 
 	return {
-		chores: await chores.json(),
-		groceries: await groceries.json(),
-		guests: guestsParsed.sort((A: Guest, B: Guest) => B.when.getDate() - A.when.getDate())
+		chores: await chores.json()
 	};
 }) as PageLoad;
--- src/routes/housework/MobileHouseworkCard.svelte
+++ src/routes/housework/MobileHouseworkCard.svelte
@@ -1,39 +1,56 @@
 <script lang='ts'>
 
-	export let item = {
-		name: '',
-		checked: false
-	};
+	import type { housework } from '../housework.type';
+
+	export let task: housework = { assigned: '', title: '', toDo: true }
 </script>
 
 <section class='card'>
-	<input type='checkbox' bind:checked={item.checked} />
-	<h2>{item.name.trim()}</h2>
+	<input type='checkbox' bind:checked={task.toDo} />
+	<h2>{task.title.trim()}</h2>
+	<span>{task.assigned}</span>
+	<hr/>
 </section>
 
 <style>
+
     h2 {
         font-weight: normal;
         font-size: 20px;
-        margin: 0;
         align-self: center;
-        margin-left: 15px;
+        margin: 0;
+        grid-area: 1 / 2 / 2 / 3;
     }
 
     .card {
         background-color: #fff;
-        border-radius: 3px;
-        box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
         padding: 10px;
         margin: 13px 0;
         height: 40px;
-        display: flex;
-        align-items: center;
+        display: grid;
+        grid-template-columns: 25px 1fr;
+        grid-template-rows: repeat(3, 1fr);
+        grid-column-gap: 20px;
+        grid-row-gap: 0px;
     }
 
     input {
         height: 25px;
         width: 25px;
+        grid-area: 1 / 1 / 2 / 2;
+    }
+
+		span {
+				font-size: 14px;
+				color: #ACACAC;
+        grid-area: 2 / 2 / 3 / 3;
+		}
+
+		hr {
+				background-color: black ;
+        grid-area: 3 / 2 / 4 / 3;
+				width: 100%;
+				border: 1px solid #efefef;
     }
 </style>
 
